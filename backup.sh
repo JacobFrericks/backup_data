fa5ab9bf43f38a195073f7ecf5b173cd9b74f654
Jacob Frericks <JacobFrericks@users.noreply.github.com>
2021-08-27 17:54:08 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,21 +1,27 @@
-##!/usr/bin/env bash
+##!/usr/bin/env bashm
 
 ## XXX: Set your bucket name here
 AWS_S3_BUCKET="Backup"
 
 ## XXX: Set your gpg encryption email address here (or remove the `gpg` line)
-GPG_EMAIL=""
-
-## Gets last argument
-filename=$1
-
-## Gets all but the last 3 arguments
-site=$2
+GPG_EMAIL=$1
 
 echo "Downloading: $filename"
 
-## Expands all args (i.e. `args[@]`) in a format that can be reused as input (i.e. `@Q`)
-cat *.txt >> all.txt
+## Expands all args (i.e. `args[@]`) in a format that can be reused as input (i.e. `@Q`
+)
+for file in *.txt; do
+  count=0
+  while read url; do
+
+    filename=${file}_${count}.${extension}
+    curl $url \
+    | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${url.txt} \
+    | aws s3 cp - "s3://${AWS_S3_BUCKET}/${filename}" \
+      --storage-class DEEP_ARCHIVE
+  done <$file
+done
+
 while read url; do
   curl $url \
     | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${url}.txt #\
