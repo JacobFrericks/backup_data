f3a49c93e3a9ccfcc6418040515412a831178caf
Jacob Frericks <jacob.frericks@gmail.com>
2021-08-28 16:21:01 -0500

    Changing how script works

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ First time use:
 1. \* Create a Glacier bucket called "backup"
 1. From a computer, with Firefox, click to download each of the files, and then cancel all of the downloads.
 1. In Firefox, go to the downloads manager, and for each cancelled download, right click, then choose “Copy Download Link”.
-1. Copy the link to the AWS EC2 instance, in a file ending in ".txt", such as "google.txt"
-1. Do the same for all services you wish to backup
+1. Copy the link to the AWS EC2 instance, in a file ending in ".txt", such as "google.txt". The first line in this file MUST be the extension used. ie "zip"
+1. Add an empty line at the end of each *.txt files (see `example.txt` for an example of what these files should look like)
+1. Do the same for all services you wish to back up
 1. Run this script in the same directory as your text files using your gpg email address as the only parameter (ie `$backup.sh myemail@gmail.com`
 1. Destroy or shut down the instance
 
--- backup.sh
+++ backup.sh
@@ -6,26 +6,33 @@ AWS_S3_BUCKET="Backup"
 ## XXX: Set your gpg encryption email address here (or remove the `gpg` line)
 GPG_EMAIL=$1
 
-echo "Downloading: $filename"
-
-## Expands all args (i.e. `args[@]`) in a format that can be reused as input (i.e. `@Q`
-)
+## Expands all args (i.e. `args[@]`) in a format that can be reused as input (i.e. `@Q`)
 for file in *.txt; do
+  echo "### $file ###"
   count=0
-  while read url; do
-
-    filename=${file}_${count}.${extension}
-    curl $url \
-    | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${url.txt} \
-    | aws s3 cp - "s3://${AWS_S3_BUCKET}/${filename}" \
-      --storage-class DEEP_ARCHIVE
-  done <$file
+  extension=""
+  while read line; do
+    if [ "$count" -eq "0" ]; then
+      extension=${line}
+      count=$((count+1))
+      continue
+    fi
+    filename=${file%.*}_${count}.${extension}
+    echo "Downloading: $filename"
+    curl ${line} \
+      | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output $filename \
+      | aws s3 cp - "s3://${AWS_S3_BUCKET}/${filename}" \
+        --storage-class DEEP_ARCHIVE
+    count=$((count+1))
+  done <${file}
 done
 
-while read url; do
-  curl $url \
-    | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${url}.txt #\
-    | aws s3 cp - "s3://${AWS_S3_BUCKET}/${filename}" \
-        --storage-class DEEP_ARCHIVE 
-done <all.txt
-rm -rf all.txt
+#while read line; do
+#  # if GPG email is not empty
+#    # cmd="gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${line}.txt"
+#  curl $line \
+#    | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${line}.txt #\
+#    | aws s3 cp - "s3://${AWS_S3_BUCKET}/${filename}" \
+#        --storage-class DEEP_ARCHIVE
+#done <all.txt
+#rm -rf all.txt
