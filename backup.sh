f36df8936d8c105dc3440f3c9bbb09f07b382a86
Jacob Frericks <jacob.frericks@gmail.com>
2021-08-28 16:33:28 -0500

    Adding a way to skip encryption if desired

--- backup.sh
+++ backup.sh
@@ -1,16 +1,22 @@
 ##!/usr/bin/env bashm
 
-## XXX: Set your bucket name here
 AWS_S3_BUCKET="Backup"
 
-## XXX: Set your gpg encryption email address here (or remove the `gpg` line)
 GPG_EMAIL=$1
+if [ -z "$GPG_EMAIL" ]; then
+  CURL_CMD="curl \${line} \
+      | aws s3 cp - \"s3://\${AWS_S3_BUCKET}/\${filename}\" \
+        --storage-class DEEP_ARCHIVE"
+else
+  CURL_CMD="curl \${line} \
+      | gpg --encrypt -r \${GPG_EMAIL} --trust-model always --output \${filename} \
+      | aws s3 cp - \"s3://\${AWS_S3_BUCKET}/\${filename}\" \
+        --storage-class DEEP_ARCHIVE"
+fi
 
-## Expands all args (i.e. `args[@]`) in a format that can be reused as input (i.e. `@Q`)
 for file in *.txt; do
   echo "### $file ###"
   count=0
-  extension=""
   while read line; do
     if [ "$count" -eq "0" ]; then
       extension=${line}
@@ -19,20 +25,7 @@ for file in *.txt; do
     fi
     filename=${file%.*}_${count}.${extension}
     echo "Downloading: $filename"
-    curl ${line} \
-      | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output $filename \
-      | aws s3 cp - "s3://${AWS_S3_BUCKET}/${filename}" \
-        --storage-class DEEP_ARCHIVE
+    eval $CURL_CMD
     count=$((count+1))
   done <${file}
 done
-
-#while read line; do
-#  # if GPG email is not empty
-#    # cmd="gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${line}.txt"
-#  curl $line \
-#    | gpg --encrypt -r ${GPG_EMAIL} --trust-model always --output ${line}.txt #\
-#    | aws s3 cp - "s3://${AWS_S3_BUCKET}/${filename}" \
-#        --storage-class DEEP_ARCHIVE
-#done <all.txt
-#rm -rf all.txt
