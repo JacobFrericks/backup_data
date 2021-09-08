a6fe213346278b49495d3393369e9a430beeb30d
Jacob Frericks <JacobFrericks@users.noreply.github.com>
2021-09-08 16:10:23 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -20,7 +20,6 @@ for file in *.txt; do
   while read line; do
     if [ "$count" -eq "0" ]; then
       extension=${line}
-      count=$((count+1))
       continue
     fi
     filename=${file%.*}_${count}.${extension}
